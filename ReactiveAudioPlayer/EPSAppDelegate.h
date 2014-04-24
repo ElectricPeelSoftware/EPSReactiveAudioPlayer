3451eaa0e0b347d3c8c8a0259ebdb87a35899b5c
Peter Stuart <peter@electricpeelsoftware.com>
2014-04-24 15:29:57 -0400

    Initial Commit

---
+++ ReactiveAudioPlayer/EPSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EPSAppDelegate.h
+//  ReactiveAudioPlayer
+//
+//  Created by Peter Stuart on 4/24/14.
+//  Copyright (c) 2014 Electric Peel, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReactiveAudioPlayer/EPSViewController.h
@@ -0,0 +1,13 @@
+//
+//  EPSViewController.h
+//  ReactiveAudioPlayer
+//
+//  Created by Peter Stuart on 4/24/14.
+//  Copyright (c) 2014 Electric Peel, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPSViewController : UIViewController
+
+@end
